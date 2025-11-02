b8d743856831c7db320fee7e915e5d1e8070283b
Nintendocustom <nintendocustomyt@gmail.com>
2025-11-02 15:05:04 +0100

    update types of numath

--- src/numath/types.h
+++ src/numath/types.h
@@ -5,3 +5,32 @@ struct NUVEC {
     float y;
     float z;
 };
+struct NUVEC4 {
+    float x;
+    float y;
+    float z;
+    float w;
+};
+struct NUMTX {
+    float m[4][4];
+};
+struct NUHALFVEC {
+    unsigned short x;
+    unsigned short y;
+    unsigned short z;
+};
+struct NUHALFVEC4 {
+    unsigned short x;
+    unsigned short y;
+    unsigned short z;
+    unsigned short w;
+};
+struct NUPLANE {
+    NUVEC normal;
+    float d;
+};
+struct NUANGVEC {
+    int x;
+    int y;
+    int z;
+};
