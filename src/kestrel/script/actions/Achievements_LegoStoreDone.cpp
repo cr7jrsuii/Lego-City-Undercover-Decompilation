fd35f13aef2638ad5a5a8323d93a329dde07379f
Nintendocustom <nintendocustomyt@gmail.com>
2025-12-30 21:45:33 +0100

    Add `SAction_Achievements_LegoStoreDone`

---
+++ src/kestrel/script/actions/Achievements_LegoStoreDone.cpp
@@ -0,0 +1,15 @@
+#include "kestrel/script/actions/Achievements_LegoStoreDone.h"
+
+const char* SAction_Achievements_LegoStoreDone::GetName() const {
+    return "Achievements_LegoStoreDone";
+}
+void SAction_Achievements_LegoStoreDone::GetInputs(SCmdParams& params) const {
+    params.SanityCheck();
+    params.AddParam(SV_FLOAT);
+}
+void SAction_Achievements_LegoStoreDone::GetOutputs(SCmdParams& params) const {
+    ActionInstanceData::GetOutputs(params);
+}
+ActionState SAction_Achievements_LegoStoreDone::Exec(ScriptContext& context) {
+    return ActionInstanceData::Exec(context);
+}
