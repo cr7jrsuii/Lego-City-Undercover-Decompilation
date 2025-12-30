7285db9cb4d2d6ef8a1468ba258c170da69673fa
Nintendocustom <nintendocustomyt@gmail.com>
2025-12-30 01:02:27 +0100

    Add `SAction_Achievements_MarkChapterComplete`

---
+++ src/kestrel/script/actions/Achievements_MarkChapterComplete.cpp
@@ -0,0 +1,15 @@
+#include "kestrel/script/actions/Achievements_MarkChapterComplete.h"
+
+const char* SAction_Achievements_MarkChapterComplete::GetName() const {
+    return "Achievements_MarkChapterComplete";
+}
+void SAction_Achievements_MarkChapterComplete::GetInputs(SCmdParams& params) const {
+    params.SanityCheck();
+    params.AddParam(SV_FLOAT);
+}
+void SAction_Achievements_MarkChapterComplete::GetOutputs(SCmdParams& params) const {
+    ActionInstanceData::GetOutputs(params);
+}
+ActionState SAction_Achievements_MarkChapterComplete::Exec(ScriptContext& context) {
+    return ActionInstanceData::Exec(context);
+}
