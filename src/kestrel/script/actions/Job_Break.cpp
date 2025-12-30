f6d363a999f2234fbd488f51ef81b6237a2273a5
Nintendocustom <nintendocustomyt@gmail.com>
2025-12-30 02:34:24 +0100

    Add `SAction_Job_Break`

---
+++ src/kestrel/script/actions/Job_Break.cpp
@@ -0,0 +1,14 @@
+#include "kestrel/script/actions/Job_Break.h"
+
+const char* SAction_Job_Break::GetName() const {
+    return "Job_Break";
+}
+void SAction_Job_Break::GetInputs(SCmdParams& params) const {
+    ActionInstanceData::GetInputs(params);
+}
+void SAction_Job_Break::GetOutputs(SCmdParams& params) const {
+    ActionInstanceData::GetOutputs(params);
+}
+ActionState SAction_Job_Break::Exec(ScriptContext& context) {
+    return ActionInstanceData::Exec(context);
+}
