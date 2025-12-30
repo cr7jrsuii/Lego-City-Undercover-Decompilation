a153d001d4467b6975501702454dfce258622b74
Nintendocustom <nintendocustomyt@gmail.com>
2025-12-30 01:55:07 +0100

    Add `SAction_CancelHint`

---
+++ src/kestrel/script/actions/CancelHint.cpp
@@ -0,0 +1,17 @@
+#include "kestrel/script/actions/CancelHint.h"
+
+void Hint_CancelCurrent(float);
+
+const char* SAction_CancelHint::GetName() const {
+    return "CancelHint";
+}
+void SAction_CancelHint::GetInputs(SCmdParams& params) const {
+    ActionInstanceData::GetInputs(params);
+}
+void SAction_CancelHint::GetOutputs(SCmdParams& params) const {
+    ActionInstanceData::GetOutputs(params);
+}
+ActionState SAction_CancelHint::Exec(ScriptContext& context) {
+    Hint_CancelCurrent(0.0f);
+    return ACTION_FINISHED;
+}
