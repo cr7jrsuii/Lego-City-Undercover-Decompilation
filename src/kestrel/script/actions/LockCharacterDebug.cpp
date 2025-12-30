b64a407d7d12aa18a25984fed47d11df6ad218d2
Nintendocustom <nintendocustomyt@gmail.com>
2025-12-30 02:38:31 +0100

    Add ` SAction_LockCharacterDebug`

---
+++ src/kestrel/script/actions/LockCharacterDebug.cpp
@@ -0,0 +1,15 @@
+#include "kestrel/script/actions/LockCharacterDebug.h"
+
+const char* SAction_LockCharacterDebug::GetName() const {
+    return "LockCharacterDebug";
+}
+void SAction_LockCharacterDebug::GetInputs(SCmdParams& params) const {
+    params.SanityCheck();
+    params.AddParam(SV_STRING);
+}
+void SAction_LockCharacterDebug::GetOutputs(SCmdParams& params) const {
+    ActionInstanceData::GetOutputs(params);
+}
+ActionState SAction_LockCharacterDebug::Exec(ScriptContext& context) {
+    return ActionInstanceData::Exec(context);
+}
