8afa5f503d3aa7f30ca97d6b7e522b07c54003d7
Nintendocustom <nintendocustomyt@gmail.com>
2026-03-11 23:11:56 +0100

    Add `SAction_GetGoldBrickTotal`

---
+++ src/kestrel/script/actions/SAction_GetGoldBrickTotal.cpp
@@ -0,0 +1,12 @@
+#include "kestrel/script/actions/SAction_GetGoldBrickTotal.h"
+
+const char* SAction_GetGoldBrickTotal::GetName() const {
+    return "SAction_GetGoldBrickTotal";
+}
+void SAction_GetGoldBrickTotal::GetInputs(SCmdParams& params) const {
+    params.SanityCheck();
+}
+void SAction_GetGoldBrickTotal::GetOutputs(SCmdParams& params) const {
+    params.SanityCheck();
+    params.AddParam(SV_NUMBER);
+}
